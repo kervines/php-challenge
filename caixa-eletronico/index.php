131f9fe836b8e87755f93a8952677365b2932bc7
Kervin Espirito Santo <dev.kervin@gmail.com>
2026-09-14 22:30:45 -0300

    fix: ajuste

--- caixa-eletronico/index.php
+++ caixa-eletronico/index.php
@@ -1,10 +1,11 @@
-<?php
+  <?php
 
 $notas = [100, 50, 20, 10, 5, 2, 1];
 
-echo "===== CAIXA ELETRÔNICO =====\n";
-echo "Qual valor deseja sacar? ";
-$valor_saque = trim(fgets(STDIN));
+echo "==================== CAIXA ELETRÔNICO ====================\n";
+echo "Cédulas disponivéis: R$100, R$50, R$20, R$10, R$5, R$2, R$1 \n";
+echo "Quanto deseja sacar? ";
+$valor_saque = (int)trim(fgets(STDIN));
 
 if($valor_saque <= 0){
   return printf('Valor inválido');
@@ -23,6 +24,6 @@ foreach ($notas as $nota) {
 
 foreach ($qnt_cedulas_valores as $valor => $qnt){
   if($qnt > 0) {
-    echo ($qnt>1) ?  $qnt . ' cédulas de ' . $valor . "\n" : $qnt . ' cédula de ' . $valor . "\n";
+    echo ($qnt>1) ?  $qnt . ' cédulas de R$' . $valor . ",00\n" : $qnt . ' cédula de R$' . $valor . ",00\n";
   }
 }
